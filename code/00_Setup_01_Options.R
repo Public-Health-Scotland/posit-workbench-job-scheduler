75c51c16f28750d0ebafee1b6765e99790fc8de5
Terry McLaughlin <terry.mclaughlin@phs.scot>
2023-01-12 10:19:06 +0000

    Rename scripts, ensure 'renv' environment is restored correctly, and add in run script to load all the functions

---
+++ code/00_Setup_01_Options.R
@@ -0,0 +1,17 @@
+################################################################################
+# Name of file:       00_Setup_01_Options.R
+# Type of script:     R
+#
+# Original author:    Terry McLaughlin
+#
+# Written/run on: R version 4.1.2 (2021-11-01) -- "Bird Hippie"
+# Platform: x86_64-pc-linux-gnu (64-bit)
+#
+# Set project-specific options
+################################################################################
+
+### 00 R options ----
+
+# Disable scientific notation
+options(scipen = 999)
+
