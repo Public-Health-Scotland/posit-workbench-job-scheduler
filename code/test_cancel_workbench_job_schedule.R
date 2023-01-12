a6f34260bc27ea78fca6a7b7932d5d7986264ba8
Terry McLaughlin <terry.mclaughlin@phs.scot>
2023-01-12 14:38:07 +0000

    Update test of 'cancel_workbench_job_schedule()'

--- code/test_cancel_workbench_job_schedule.R
+++ code/test_cancel_workbench_job_schedule.R
@@ -11,18 +11,7 @@
 # created by the function 'schedule_workbench_job()'.
 ################################################################################
 
-### 00 Required packages ----
-
-#install.packages("here")
-#install.packages("lubridate")
-
-### 01 Source function script ----
-
-source(file.path(here::here(), "code/launch_workbench_job.R"))
-source(file.path(here::here(), "code/schedule_workbench_job.R"))
-source(file.path(here::here(), "code/cancel_workbench_job_schedule.R"))
-
-### 02 Schedule multiple Workbench Jobs on multiple schedules ----
+### 00 Schedule multiple Workbench Jobs on multiple schedules ----
 
 # Schedule multiple Workbench Jobs on multiple schedules
 for(i in seq(1, 9)){
@@ -30,7 +19,7 @@ for(i in seq(1, 9)){
     job_name = paste0("job_name_", i),
     schedule_name = paste0("schedule_", i),
     due = lubridate::now() + lubridate::minutes(1),
-    rpt = as.numeric(lubridate::as.duration("5 minutes")),
+    rpt = as.numeric(lubridate::as.duration("1 minute")),
     project_path = here::here(),
     script = "code/test_launch_workbench_job_script.R",
     n_cpu = 0.25,
@@ -43,14 +32,22 @@ for(i in seq(1, 9)){
 later::later(function() {View(scheduled_workbench_jobs)}, 10)
 
 
+# View the tibble (1 minute and 10 seconds later) containing the details of the scheduled
+# workbench jobs
+later::later(function() {View(scheduled_workbench_jobs)}, 70)
+
+
 ### 03 Cancel some of the schedules ----
 
-for(i in seq(3, 5)){
-  cancel_workbench_job_schedule(paste0("schedule_", i))
-}
+later::later(function() {
+  for(i in seq(3, 5)){
+    cancel_workbench_job_schedule(paste0("schedule_", i))
+  }
+}, 120)
+
 
 # View the tibble (10 seconds later) containing the details of the scheduled
 # workbench jobs
-later::later(function() {View(scheduled_workbench_jobs)}, 10)
+later::later(function() {View(scheduled_workbench_jobs)}, 130)
 
 
