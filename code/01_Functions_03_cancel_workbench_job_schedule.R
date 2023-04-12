202b6129033e2c3a12d4de7d7c2edcee2a8376b3
Terry McLaughlin <terry.mclaughlin@phs.scot>
2023-04-12 15:39:34 +0100

    Remove {cli} function calls from code/01_Functions_03_cancel_workbench_job_schedule.R

--- code/01_Functions_03_cancel_workbench_job_schedule.R
+++ code/01_Functions_03_cancel_workbench_job_schedule.R
@@ -14,24 +14,20 @@ cancel_workbench_job_schedule <- function(schedule_name){ # The name of the sche
   
   # schedule_name - should be a string (one element character vector)
   if(!inherits(schedule_name, "character")){
-    cli::cli_abort(c(
-      "{.var schedule_name} must be an object of class {.cls {class('character')}}.",
-      "x" = "{.var schedule_name} is an object of {.cls {class(schedule_name)}}."))
+    write_stderr("✖ schedule_name must be an object of class 'character'.\n")
+    stop("schedule_name is an object of class ", class(schedule_name), ".")
   } else{
     if (!length(schedule_name) == 1){
-      cli::cli_abort(c(
-        "{.var schedule_name} must be a {.cls {class('character')}} vector of length 1.",
-        "x" = paste0("{.var schedule_name} is a {.cls {class(schedule_name)}} vector of length ", length(schedule_name), ".")))
+      write_stderr("✖ schedule_name must be a 'character' vector of length 1.\n")
+      stop("schedule_name is a", class(schedule_name), " vector of length ", length(schedule_name), ".")
     } else{
       if(!exists(schedule_name, envir = .GlobalEnv)){
-        cli::cli_abort(c(
-          "{.var schedule_name} must match the name of an object in the .GlobalEnv.",
-          "x" = paste0("An object with the name '", schedule_name, "' does not exist in the .GlobalEnv.")))
+        write_stderr("✖ schedule_name must match the name of an object in the .GlobalEnv.\n")
+        stop("An object with the name '", schedule_name, "' does not exist in the .GlobalEnv.")
       } else{
         if(class(get(schedule_name, envir = .GlobalEnv)) != class(later::global_loop())){
-          cli::cli_abort(c(
-            "{.var schedule_name} must match the name of an object of class {.cls {class(later::global_loop())}}.",
-            "x" = paste0("The object '", schedule_name, "' in the Global Environnment is of class {.cls {class(get(schedule_name, envir = .GlobalEnv))}}.")))
+          write_stderr("✖ schedule_name must match the name of an object of class ", class(later::global_loop()), "./n")
+          stop("The object '", schedule_name, "' in the Global Environnment is of class ", class(get(schedule_name, envir = .GlobalEnv)), ".")
         }
       }
     }
@@ -45,24 +41,20 @@ cancel_workbench_job_schedule <- function(schedule_name){ # The name of the sche
   # If the 'schedule_name' event loop's status is 'destroyed', remove the
   # 'schedule_name' object from GlobalEnv
   if(!later::exists_loop(get(schedule_name, envir = .GlobalEnv))) {
-    cli::cli_inform(c(
-      "v" = paste0("The schedule '", schedule_name, "' has successfully been cancelled."),
-      "i" = paste0("No further Workbench Jobs that were scheduled on '", schedule_name, "' will be launched.")
-    ))
+    write_stdout("✔ The schedule '", schedule_name, "' has successfully been cancelled.\n")
+    write_stdout("ℹ No further Workbench Jobs that were scheduled on '", schedule_name, "' will be launched.")
+    
     rm(list = as.character(schedule_name), envir = .GlobalEnv)
   } else{
-    cli::cli_abort(c(
-      paste0("Unable to remove {.var ", schedule_name, "} from {.code .GlobalEnv}."),
-      "x" = paste0("The event loop {.var ", schedule_name, "} still exists."),
-      "i" = paste0("Ensure that the event loop is destroyed first: {.code later::destroy_loop(", schedule_name, ")}.")))
+    write_stderr("✖ The event loop ", schedule_name, " still exists.\n")
+    write_stderr("ℹ Ensure that the event loop is destroyed first: later::destroy_loop(", schedule_name, ").\n")
+    stop("Unable to remove ", schedule_name, " from .GlobalEnv.")
   }
   
   # Inform the user of the remaining schedules
   r <- lapply(ls(envir = .GlobalEnv), function(x){
     if(class(get(x, envir = .GlobalEnv)) == class(later::global_loop())){
-        cli::cli_inform(c(
-          "i" = paste0("The schedule '", x, "' is still active.")
-        ))
+      write_stdout("ℹ The schedule '", x, "' is still active.\n")
     }
   })
 }
