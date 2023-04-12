adbe2dbcba740ed7b758b3b8854247785e9d6612
Terry McLaughlin <terry.mclaughlin@phs.scot>
2023-04-12 10:40:45 +0100

    Define R functions that output to stdout and stderr
    These functions provide a more flexible way of outputting to stdout and stderr, allowing a variable number of arguments to be passed to the functions in the same way as printf().

--- code/99_Run.R
+++ code/99_Run.R
@@ -23,14 +23,17 @@ source(file.path(here::here(), "code", "00_Setup_01_Options.R"))
 # Define file paths
 source(file.path(here::here(), "code", "00_Setup_02_Paths.R"))
 
+# Define functions to write to stdout and stderr
+source(file.path(here::here(), "code", "01_Functions_00_write_output_to_stdout_or_stderr.R"))
+
 # Define launch_workbench_job()
-source(file.path(here::here(), "code", "01_Functions_00_launch_workbench_job.R"))
+source(file.path(here::here(), "code", "01_Functions_01_launch_workbench_job.R"))
 
 # Define schedule_workbench_job()
-source(file.path(here::here(), "code", "01_Functions_01_schedule_workbench_job.R"))
+source(file.path(here::here(), "code", "01_Functions_02_schedule_workbench_job.R"))
 
 # Define cancel_workbench_job_schedule()
-source(file.path(here::here(), "code", "01_Functions_02_cancel_workbench_job_schedule.R"))
+source(file.path(here::here(), "code", "01_Functions_03_cancel_workbench_job_schedule.R"))
 
 #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
